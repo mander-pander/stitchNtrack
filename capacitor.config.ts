5525a8368e24fe0b601a7f0d039cd7b727357e3e
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-03 14:43:27 -0800

    server folder

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Stitch-N-Track',
+  appName: 'myApp',
   webDir: 'build',
   bundledWebRuntime: false
 };
