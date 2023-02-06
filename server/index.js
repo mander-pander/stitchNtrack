f83410baa899977937e740260355bee165370af1
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-06 15:52:49 -0800

    connect database, created trial query

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
-
+const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from Express!" });
+require('dotenv').config();
+
+const mysql = require('mysql2');
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+connection.connect();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/users", (req, res) => {
+    connection.query('SELECT * FROM users', function (err, rows, fields) {
+        if(err) throw err;
+        res.send(rows);
+    });
 });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+console.log('Connected to PlanetScale!');
