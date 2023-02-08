7309ce6529f57c59c4f1b15ba00a98f557d58705
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-08 09:51:04 -0800

    mapp projects

--- app/src/pages/Tab1.tsx
+++ app/src/pages/Tab1.tsx
@@ -4,13 +4,12 @@ import { useState , useEffect} from 'react';
 import axios from 'axios';
 
 const Tab1: React.FC = () => {
-  const [data, setData] = useState(null);
+  const [projects, setProjects] = useState<any[]>([]);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/project`)
     .then((result: any) => {
-      setData(result.data[0].name)
-      console.log(result.data)
+      setProjects(result.data);
     },
     (error) => {
       console.log(error);
@@ -27,10 +26,14 @@ const Tab1: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Projects</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <p>{data}</p>
+          {projects.map((project) => (
+          <div>
+              <li>Project name: {project.name}</li>
+          </div>
+        ))}
       </IonContent>
     </IonPage>
   );
