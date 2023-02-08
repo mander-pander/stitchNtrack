70695816361c34df6f5716decb72d66f14f15331
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-08 12:57:17 -0800

    project get query

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require('cors');
-const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -10,20 +9,31 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-connection.connect();
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to PlanetScale!");
+});
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/users", (req, res) => {
-    connection.query('SELECT * FROM users', function (err, rows, fields) {
-        if(err) throw err;
+    connection.query('SELECT * FROM users', function (err, rows) {
+        if (err) throw err;
         res.send(rows);
+    });
+});
+
+app.get("/project", (req, res) => {
+    connection.query(
+        "SELECT * FROM project",
+    function (err, rows) {
+        if (err) throw err;
+        res.send(rows);
+        console.log(rows);
     });
 });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
-
-console.log('Connected to PlanetScale!');
