5904fcc4e71ade44b366d64b46d16c0884515e54
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-08 08:27:50 -0800

    axios get request for project working

--- app/src/pages/Tab1.tsx
+++ app/src/pages/Tab1.tsx
@@ -1,15 +1,17 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import { useState , useEffect} from 'react';
+import axios from 'axios';
 
 const Tab1: React.FC = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(`http://localhost:3001/users`)
-    .then(res => res.json())
-    .then((result) => {setData(result[0])},
+    axios.get(`http://localhost:3001/project`)
+    .then((result: any) => {
+      setData(result.data[0].name)
+      console.log(result.data)
+    },
     (error) => {
       console.log(error);
     })
