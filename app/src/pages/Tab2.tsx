aa161906c3e35547089f294b0735c2755e5c5f09
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-09 14:00:01 -0800

    request sent with post

--- app/src/pages/Tab2.tsx
+++ app/src/pages/Tab2.tsx
@@ -1,15 +1,32 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
-  const [project, setProject] = useState('');
+  const [project, setProject] = useState({
+    name: '',
+    yarn_id: 0,
+    needleSize: 0,
+    gauge: 0,
+    patternRepeat_id: 0,
+    user_id: 0
+  });
 
-  async function handleAddProject(e: any) {
+  const handleAddProject = async(e: any) => {
     e.preventDefault();
-    let res = await axios.post('http://localhost:3001/project')
+    let res = await axios.post('http://localhost:3001/project', {
+      project
+    })
     console.log(res);
+    console.log(project)
+  }
+
+  const handleChange = (e: any) => {
+    setProject({
+      ...project,
+      [e.target.name]: e.target.value
+    });
   }
 
   return (
@@ -27,23 +44,21 @@ const Tab2: React.FC = () => {
         </IonHeader>
         <form onSubmit={handleAddProject}>
           <label>Project Name:
-            <input type="text" />
+            <input type="text" name="name" value={project.name}  onChange={handleChange}/>
           </label>
           <label>Yarn:
-            <input type="text" />
+            <input type="text" name="yarn_id" value={project.yarn_id} onChange={handleChange}/>
           </label>
           <label>Needle/Hook Size:
-            <input type="number" />
+            <input type="number" name="needleSize" value={project.needleSize} onChange={handleChange}/>
           </label>
           <label>Gauge:
-            <input type="number" />
+            <input type="number" name="gauge" value={project.gauge} onChange={handleChange}/>
           </label>
           <label>Pattern Repeats:
-            <input type="text" />
+            <input type="text" name="patternRepeat_id" value={project.patternRepeat_id} onChange={handleChange}/>
           </label>
-          <label>Date Started:
-            <input type="date" />
-          </label>
+
           <input type="submit" />
         </form>
       </IonContent>
