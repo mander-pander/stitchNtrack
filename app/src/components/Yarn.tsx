44574fd4f9299864eda8b02ad68781d4ac2a9f00
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-14 08:10:15 -0800

    imported components for different parts of form for new project, passing yarn info through props

--- app/src/components/Yarn.tsx
+++ app/src/components/Yarn.tsx
@@ -2,43 +2,29 @@ import { useState } from 'react';
 import { add } from 'ionicons/icons';
 import { IonIcon } from '@ionic/react';
 
-const Yarn = () => {
+const Yarn = ({yarn_name, weight, yardage, color, onChange}:any) => {
     const [show, setShow] = useState(false);
 
     const toggleShow = () => {
         setShow(!show);
     };
 
-    const [yarn, setYarn] = useState({
-        yarn_name: '',
-        weight: 0,
-        yardage: 0,
-        color: '',
-        project_id: 0
-    });
-
-    const handleYarnChange = (e: any) => {
-        setYarn({
-            ...yarn,
-            [e.target.name]: e.target.value
-        });
-    };
     return (
         <div>
             <IonIcon icon={add} onClick={toggleShow} /> Yarn Info:
             {show ?
                 <div>
                     <label> Name:
-                        <input type="text" name="yarn_name" value={yarn.yarn_name} onChange={handleYarnChange} />
+                        <input type="text" name="yarn_name" value={yarn_name} onChange={onChange} />
                     </label>
                     <label> weight:
-                        <input type="number" name="weight" value={yarn.weight} onChange={handleYarnChange} />
+                        <input type="number" name="weight" value={weight} onChange={onChange} />
                     </label>
                     <label> Yardage:
-                        <input type="number" name="yardage" value={yarn.yardage} onChange={handleYarnChange} />
+                        <input type="number" name="yardage" value={yardage} onChange={onChange} />
                     </label>
                     <label> Color:
-                        <input type="text" name="color" value={yarn.color} onChange={handleYarnChange} />
+                        <input type="text" name="color" value={color} onChange={onChange} />
                     </label>
                 </div>
             : null}
--- app/src/pages/Tab2.tsx
+++ app/src/pages/Tab2.tsx
@@ -2,8 +2,14 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import { useState } from 'react';
 import axios from 'axios';
 import './Tab2.css';
+import Yarn from '../components/Yarn';
+import Tool from '../components/Tool';
+import Gauge from '../components/Gauge';
+import Date from '../components/Date';
+
 
 const Tab2: React.FC = () => {
+
   const [project, setProject] = useState({
     name: '',
     needle_size: 0,
@@ -19,7 +25,7 @@ const Tab2: React.FC = () => {
     yardage: 0,
     color: '',
     project_id: 0
-  });
+});
 
   const handleAddProject = async(e: any) => {
     e.preventDefault();
@@ -28,7 +34,7 @@ const Tab2: React.FC = () => {
     })
     console.log(res);
     console.log(project)
-    console.log("yarn info", yarn)
+    console.log("yarn info")
   }
 
   const handleProjectChange = (e: any) => {
@@ -40,10 +46,10 @@ const Tab2: React.FC = () => {
 
   const handleYarnChange = (e: any) => {
     setYarn({
-      ...yarn,
-      [e.target.name]: e.target.value
+        ...yarn,
+        [e.target.name]: e.target.value
     });
-  }
+  };
 
   return (
     <IonPage>
@@ -59,35 +65,16 @@ const Tab2: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <form onSubmit={handleAddProject}>
+
           <label>Project Name:
             <input type="text" name="name" value={project.name}  onChange={handleProjectChange}/>
           </label>
-          <label>Needle/Hook Size:
-            <input type="number" name="needle_size" value={project.needle_size} onChange={handleProjectChange}/>
-          </label>
-          <label>Yarn Info:
-            <label> Name:
-              <input type="text" name="yarn_name" value={yarn.yarn_name} onChange={handleYarnChange}/>
-            </label>
-            <label> weight:
-              <input type="number" name="weight" value={yarn.weight} onChange={handleYarnChange}/>
-            </label>
-            <label> Yardage:
-              <input type="number" name="yardage" value={yarn.yardage} onChange={handleYarnChange}/>
-            </label>
-            <label> Color:
-              <input type="text" name="color" value={yarn.color} onChange={handleYarnChange}/>
-            </label>
-          </label>
-          <label>Gauge:
-            <input type="number" name="gauge" value={project.gauge} onChange={handleProjectChange}/>
-          </label>
-          <label>Start Date:
-            <input type="date" name="date_started" value={project.date_started} onChange={handleProjectChange}/>
-          </label>
-          <label>Finish Date:
-            <input type="date" name="date_finished" value={project.date_finished} onChange={handleProjectChange}/>
-          </label>
+
+          <Tool needle_size={project.needle_size} onChange={handleProjectChange} />
+          <Yarn yarn_name={yarn.yarn_name} weight={yarn.weight} yardage={yarn.yardage} color={yarn.color} onChange={handleYarnChange}/>
+          <Gauge gauge={project.gauge} onChange={handleProjectChange} />
+          <Date date_started={project.date_started} date_finished={project.date_finished} onChange={handleProjectChange} />
+
           <input type="submit" />
         </form>
       </IonContent>
