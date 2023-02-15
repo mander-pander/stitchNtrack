63fd6c0d78d8026d6b64158f713c91a82c878058
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-14 17:51:21 -0800

    pop up modal functioning for each project

--- app/src/pages/Tab1.tsx
+++ app/src/pages/Tab1.tsx
@@ -1,19 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonItem, IonButtons, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonGrid, IonRow, IonCol, IonModal } from '@ionic/react';
 import './Tab1.css';
-import { useState , useEffect} from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import { add } from 'ionicons/icons';
 
 const Tab1: React.FC = () => {
   const [projects, setProjects] = useState<any[]>([]);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/project`)
-    .then((result: any) => {
-      setProjects(result.data);
-    },
-    (error) => {
-      console.log(error);
-    })
+      .then((result: any) => {
+        setProjects(result.data);
+      },
+        (error) => {
+          console.log(error);
+        })
   }, [])
 
   return (
@@ -23,20 +24,46 @@ const Tab1: React.FC = () => {
           <IonTitle>Projects</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent className="ion-padding" >
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Projects</IonTitle>
           </IonToolbar>
         </IonHeader>
-          {projects.map((project) => (
-          <div>
-              <li>Project name: {project.name}</li>
-          </div>
-        ))}
+        {projects.map((project) => <TriggerWithModal project={project} />)}
       </IonContent>
     </IonPage>
   );
 };
 
 export default Tab1;
+
+function TriggerWithModal(props: any) {
+  let { project } = props;
+  const modal = useRef<HTMLIonModalElement>(null);
+
+  return (
+    <>
+      <div key={project.id} id={`open-modal-${project.id}`}>
+          <p >{project.name}</p>
+      </div>
+      <IonModal ref={modal} trigger={`open-modal-${project.id}`}>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonButton onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
+            </IonButtons>
+            <IonTitle>Welcome</IonTitle>
+            <IonButtons slot="end">
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonItem>
+            <p>modal info</p>
+          </IonItem>
+        </IonContent>
+      </IonModal>
+    </>
+  )
+}
