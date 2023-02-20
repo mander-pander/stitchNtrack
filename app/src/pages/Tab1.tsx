0d5d8690b7ffcfd19d2866de30d05347b1aae989
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-20 08:55:10 -0800

    edit state added and operational

--- app/src/pages/Tab1.tsx
+++ app/src/pages/Tab1.tsx
@@ -1,8 +1,8 @@
-import { IonItem, IonButtons, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonGrid, IonRow, IonCol, IonModal } from '@ionic/react';
-import './Tab1.css';
+import { IonGrid, IonRow, IonCol, IonIcon, IonButtons, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonModal } from '@ionic/react';
+import styles from './Tab1.module.css';
+import { documentTextOutline } from 'ionicons/icons';
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import { add } from 'ionicons/icons';
 
 const Tab1: React.FC = () => {
   const [projects, setProjects] = useState<any[]>([]);
@@ -38,31 +38,60 @@ const Tab1: React.FC = () => {
 
 export default Tab1;
 
-function TriggerWithModal(props: any) {
-  let { project } = props;
+function TriggerWithModal({ project }: any) {
   const modal = useRef<HTMLIonModalElement>(null);
+  const [edit, setEdit] = useState(false);
 
   return (
     <>
-      <div key={project.id} id={`open-modal-${project.id}`}>
-          <p >{project.name}</p>
-      </div>
+      <IonGrid className={styles.projectItem}>
+        <div key={project.id}>
+          <IonRow>
+            <IonCol className={styles.projectInfo} size="auto"><IonIcon id={`open-modal-${project.id}`} icon={documentTextOutline} /> </IonCol>
+            <IonCol className={styles.projectInfo}><p >{project.name}</p> </IonCol>
+          </IonRow>
+        </div>
+      </IonGrid>
       <IonModal ref={modal} trigger={`open-modal-${project.id}`}>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonButton onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
-            </IonButtons>
-            <IonTitle>Welcome</IonTitle>
-            <IonButtons slot="end">
-            </IonButtons>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className="ion-padding">
-          <IonItem>
-            <p>modal info</p>
-          </IonItem>
-        </IonContent>
+        {!edit &&
+          <>
+            <IonHeader>
+              <IonToolbar>
+                <IonButtons slot="start">
+                  <IonButton onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
+                </IonButtons>
+                <IonTitle>{project.name}</IonTitle>
+                <IonButtons slot="end">
+                  <IonButton onClick={() => setEdit(!edit)}>Edit</IonButton>
+                </IonButtons>
+              </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+              <p>Needle or Hook Size: {project.needle_size}</p>
+              <p>Gauge: {project.gauge}</p>
+              <p>Date Started: {project.date_started}</p>
+              <p>Date Finished: {project.date_finished}</p>
+            </IonContent>
+          </>
+        }
+        {edit &&
+          <>
+            <IonHeader>
+              <IonToolbar>
+                <IonButtons slot="start">
+                  <IonButton onClick={() => setEdit(false)}>Cancel</IonButton>
+                </IonButtons>
+                <IonTitle>{project.name}</IonTitle>
+              </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+              <p>Needle or Hook Size: </p> <input value={project.needle_size}/>
+              <p>Gauge: </p> <input value={project.gauge}/>
+              <p>Date Started: </p> <input value={project.date_started} />
+              <p>Date Finished: </p> <input value={project.date_finished} />
+            </IonContent>
+          </>
+        }
       </IonModal>
     </>
   )
