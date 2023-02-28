409228ae0d68b1abc52ec4b21393321b87a194a4
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-28 09:50:05 -0800

    popup modal added

--- app/src/pages/Tab4.tsx
+++ app/src/pages/Tab4.tsx
@@ -1,6 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonLabel, IonItem } from '@ionic/react';
-import { useState, useEffect } from 'react';
+import { IonModal, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonIcon, IonButtons, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList} from '@ionic/react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import { trashOutline} from 'ionicons/icons';
 
 
 const Tab4: React.FC = () => {
@@ -30,7 +31,7 @@ const Tab4: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonList inset={true}>
-          {yarns.map((yarn) => <IonItem><IonLabel> {yarn.name} </IonLabel></IonItem>)}
+          {yarns.map((yarn) => <TriggerWithModal yarn={yarn} />)}
         </IonList>
       </IonContent>
     </IonPage>
@@ -38,3 +39,49 @@ const Tab4: React.FC = () => {
 };
 
 export default Tab4;
+
+function TriggerWithModal({ yarn }: any) {
+  const modal = useRef<HTMLIonModalElement>(null);
+
+  const handleDelete = async (yarn_id: number) => {
+    let params = {data: {yarn_id}};
+      let res = await axios.delete(`http://localhost:3001/yarn/`, {
+        params
+      });
+      console.log(res);
+  }
+
+  return (
+    <>
+      <div key={yarn.id}>
+        <IonCard id={`open-modal-${yarn.id}`} >
+          <IonCardHeader>
+            <IonCardTitle>
+              {yarn.name}
+            </IonCardTitle>
+            <IonCardContent>
+              optional content here
+            </IonCardContent>
+          </IonCardHeader>
+        </IonCard>
+      </div>
+      <IonModal ref={modal} trigger={`open-modal-${yarn.id}`}>
+          <>
+            <IonHeader>
+              <IonToolbar>
+                <IonButtons slot="start">
+                  <IonButton onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
+                </IonButtons>
+                <IonTitle>{yarn.name}</IonTitle>
+              </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+              <p>Weight: {yarn.weight}</p>
+
+              <IonIcon icon={trashOutline} onClick={() => handleDelete(yarn.id)}/>
+            </IonContent>
+          </>
+      </IonModal>
+    </>
+  )
+}
